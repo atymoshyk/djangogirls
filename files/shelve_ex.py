498a81e3f0b82c926eb7904ab450ffb5a5512695
Andrii Tymoshyk <andrii.tymoshyk@gmail.com>
2017-01-19 16:27:01 +0200

    Changede location

--- files/shelve_ex.py
+++
@@ -1,7 +0,0 @@
-import shelve
-
-shelfFile = shelve.open('mydata')
-cats = ['Zophie,', 'Murka', 'Simon']
-shelfFile['cats'] = cats
-shelfFile['somevalue'] = 'somevalue'
-shelfFile.close()
--- files/shelve_read.py
+++
@@ -1,6 +0,0 @@
-import shelve
-
-shelfFile = shelve.open('mydata')
-print(shelfFile['cats'])
-print(shelfFile['somevalue'])
-shelfFile.close()
