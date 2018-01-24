aea3b152152cd35a173e75f9713f51ddaf3cf0d4
Andrii Tymoshyk <andrey.timoshik@gmail.com>
2018-01-24 23:06:10 +0200

    Debug to false

--- atym1st/settings.py
+++ atym1st/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g^90ulyjfnpouwd5+=v-8tv&lb9emvm!8y5&cdfupk3^!e0^q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'atymoshyk.pythonanywhere.com']
 
