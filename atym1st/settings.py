223f58035bf2f8a075eba13398ad14e02820ca3a
Andrii Tymoshyk <andrey.timoshik@gmail.com>
2018-01-26 09:51:37 +0200

    set english lang

--- atym1st/settings.py
+++ atym1st/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'uk-ua'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Kiev'
 
