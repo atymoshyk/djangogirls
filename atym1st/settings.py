1d560b83ee5b075af40867b49e3fefdd7e65a02e
Andrii Tymoshyk <andrey.timoshik@gmail.com>
2018-02-09 00:19:55 +0200

    Use remote postgres DB

--- atym1st/settings.py
+++ atym1st/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'atym1st.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'PORT': '5432',
     }
 }
 
